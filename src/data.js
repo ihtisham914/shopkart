ba12c65748696e952e6420dc5c3bd5be163d5c8a
Ihtisham Ul haq <you@example.com>
2022-12-06 19:37:33 +0500

    initial commit, navbar and slider added

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "/images/summer.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "/images/winter.png",
+    title: "WINTER COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "/images/popular.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+  {
+    id: 4,
+    img: "/images/frok.png",
+    title: "LOUGNEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% FOR NEW ARRIVALS.",
+    bg: "fbf0f7",
+  },
+];
